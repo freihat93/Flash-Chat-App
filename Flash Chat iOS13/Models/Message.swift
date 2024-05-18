123ba48c3055411ad2c8869d61b93e8393f62cf2
Mohammad Freihat <56158246+freihat93@users.noreply.github.com>
2024-05-18 11:56:58 +0300

    Commit Flash Chat App

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Mohammad on 5/13/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+
+struct Message {
+    let sender: String
+    let body: String
+}
